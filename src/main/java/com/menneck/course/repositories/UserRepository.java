52a65a8ac05191874f36ed14eb91c21ffde9bd6c
menneck <zeeeee.peeeeetryyy@gmail.com>
2023-01-14 17:53:32 -0300

    Service layer, component registration

--- src/main/java/com/menneck/course/repositories/UserRepository.java
+++ src/main/java/com/menneck/course/repositories/UserRepository.java
@@ -2,9 +2,10 @@ package com.menneck.course.repositories;
 
 import com.menneck.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
 
 
 }
