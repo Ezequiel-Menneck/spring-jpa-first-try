95a5852f7aa903e1499b5a7b7d4c457dd1b3d7e7
menneck <zeeeee.peeeeetryyy@gmail.com>
2023-01-15 17:57:55 -0300

    Create Product entity, repository, resource and service

---
+++ src/main/java/com/menneck/course/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.menneck.course.services;
+
+import com.menneck.course.entities.Product;
+import com.menneck.course.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Product findById(Long id) {
+        Optional<Product> obj = productRepository.findById(id);
+        return obj.get();
+    }
+
+}
