11a58fcc34d53d567a0ac4ef11e2cb24b0bda4d5
menneck <zeeeee.peeeeetryyy@gmail.com>
2023-01-16 22:20:53 -0300

    Exception handling update

--- src/main/java/com/menneck/course/services/UserService.java
+++ src/main/java/com/menneck/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.menneck.course.entities.User;
 import com.menneck.course.repositories.UserRepository;
 import com.menneck.course.services.exceptions.DatabaseException;
 import com.menneck.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -42,9 +43,13 @@ public class UserService {
     }
 
     public User update(Long id, User user) {
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, user);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, user);
+            return userRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User user) {
